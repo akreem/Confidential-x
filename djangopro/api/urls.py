bd5d3700bafa6acd6405286b8964d89769aac2f7
Akram Issaoui <akrem.issaoui@etudiant-isi.utm.tn>
2024-03-12 18:25:52 +0100

    Delete djangopro/api directory

--- djangopro/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("personnes/<param>", views.personnes, name="personnes"),
-    path("personnes/", views.personnesAPI, name="personnesAPI"),
-    path("enqext/<cin>", views.enqext, name="enqext")
-
-]
