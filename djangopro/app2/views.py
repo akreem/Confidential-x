0ece138bc2a3463318b5cc3a17529d34947da240
Akram Issaoui <akrem.issaoui@etudiant-isi.utm.tn>
2024-03-12 18:26:14 +0100

    Delete djangopro/app2 directory

--- djangopro/app2/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render, redirect
-from django.http import HttpResponse, HttpResponseRedirect
-
-# Create your views here.
-def main(request):
-
-    return HttpResponseRedirect('s4')
